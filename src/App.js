0e1078a3938b85060e92d87c35e828c1b0976837
Draeruiz <eruiz@aguascalientes.tecnm.mx>
2021-08-13 09:02:43 -0500

    Se modificaron App.s y Header se agregó funcionalidad para agregar recordatorio

--- src/App.js
+++ src/App.js
@@ -4,64 +4,59 @@ import {useState} from 'react'
 import Header from './components/Header'
 import Tasks from './components/Tasks'
 import AddTask from './components/AddTask'
-/* En esta parte del código se agrega un hook useState que es una función que epermite acceder alas funcionalida des de React
-  useState es para añadir un etado interno a nuestros componentes par ahacerlos dináicos e interactivos
-*/
+
 function App() {
+  const [showAddTask, setShowAddTask]= useState(false)
 const [tasks, setTasks] = useState([
 {
   id:1,
   text:'Cita con el Doctor',
-  day: '12 de Agosto 2021',
+  day: '25 de Agosto 2021',
   reminder: true
 },
 {
   id:2,
   text:'Videoconferencia ITA',
   day: '3 de Septiembre 2021',
-  reminder: true
+  reminder: false
 },
 {
   id:3,
   text:'Entrega de Avance de Proyecto',
   day: '15 de Agosto 2021',
-  reminder: false
-  },
-  {
-    id:4,
-    text:'Entregar instrumentación didáctica',
-    day: '20 de Agosto 2021',
-    reminder: false
-    }
+  reminder: true
+  }
 ])
 
-const deleteTask=(id)=>{
- // console.log('Delete',id);
- setTasks(tasks.filter((task)=>task.id!==id))
+
+const deleteTask = (id) =>{
+  //console.log('delete', id);
+  setTasks(tasks.filter((task) => task.id !== id))
 }
-const toggleReminder=(id)=>{
-   console.log('Reminder',id);
-  //setTasks(tasks.filter((task)=>task.id!==id))
-  //Cambiar formado al hacer doble click
-setTasks(tasks.map((task)=>task.id===id ? {...task,reminder:!task.reminder} : task))
 
- }
- const addTask=(task)=>{
-   console.log(task);
+const toggleReminder = (id) =>{
+    //console.log('Reminder', id);
+    setTasks(tasks.map((task) => task.id === id ? {...task,reminder:!task.reminder} : task))
+}
 
- }
+const addTask = (task) =>{
+   //console.log(task);
+   const id = Math.floor(Math.random() * 10000) + 1
+   const newTask = {id, ...task}
+   setTasks([...tasks, newTask])
+}
 
 
-const nombre = 'Elvia Ruiz Beltrán'
-const x = true
-const valor=10
+const nombre = 'Ricardo'
+const x = !true
+const y = !false
 
   return (
     <div>
-   
-    <Header title={'Recordatorios de ' + nombre}/>
-    <AddTask onAdd={ addTask}/>
-    <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder}/>
+    <Header title='Recordatorios' onAdd={() => setShowAddTask(!showAddTask)} showAdd={showAddTask} />
+
+    {showAddTask && <AddTask onAdd={addTask} />}
+    <Tasks tasks={tasks} onDelete={deleteTask} onToggle={toggleReminder} />
 
     </div>
   );
